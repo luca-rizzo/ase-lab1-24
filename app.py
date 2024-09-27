e6917f2a9aaf389d2f432f7741a0cd836caaca8b
Luca Rizzo <luke@DESKTOP-VAO4FSQ>
2024-09-27 12:06:41 +0200

    normal assignment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, jsonify
+import random
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -12,14 +13,54 @@ def add():
         return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 #Endpoint /sub for subtraction which takes a and b as query parameters.
+@app.route('/sub')
+def sub():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a-b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 #Endpoint /mul for multiplication which takes a and b as query parameters.
-
+@app.route('/mul')
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a*b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+    
 #Endpoint /div for division which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
+@app.route('/div')
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b and b != 0:
+        return make_response(jsonify(s=a/b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 #Endpoint /mod for modulo which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
-
+@app.route('/mod')
+def mod():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b and b != 0:
+        return make_response(jsonify(s=a%b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+    
 #Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+@app.route('/random')
+def randomGen():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b and a <= b:
+        return make_response(jsonify(s=random.randint(a, b)), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 if __name__ == '__main__':
     app.run(debug=True)
